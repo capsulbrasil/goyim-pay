fff08b16a24f0e92b24f9ac21fdc35129868aab8
ringeringeraja <joaosan177@gmail.com>
2023-09-24 01:39:39 -0300

    refactor: remove js files

--- packages/core/src/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defineConfig = void 0;
-var defineConfig = function (config) {
-    return config;
-};
-exports.defineConfig = defineConfig;
--- packages/core/src/waterfal.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.waterfal = void 0;
-var waterfal = function () {
-};
-exports.waterfal = waterfal;
